6a68a2760ed3e4a2c80e1229dd85750481d7524a
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-06-24 13:38:58 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -4,8 +4,6 @@
 
 int printColorMap(void(*ReferenceManualFunPtr)(int,int)) 
 {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) 
     {
@@ -18,7 +16,12 @@ int printColorMap(void(*ReferenceManualFunPtr)(int,int))
     return i * j;
 }
 
-void TestPrintColorMap(int MajorColorIndex, intMinorColorIndex)
+void print(int MajorColorIndex, int MinorColorIndex)
+{
+    printf("%d | %s | %s\n", MajorColorIndex * 5 + MinorColorIndex+ 1, majorColor[MajorColorIndex], minorColor[MajorColorIndex]);
+}
+
+void TestPrintColorMap(int MajorColorIndex, int MinorColorIndex)
 {
     int k= 1;
     Manual[k].PairNumber = (MajorColorIndex*5)+ MinorColorIndex+1;
@@ -32,9 +35,9 @@ int main()
     ReferenceManualFunPtr = &TestPrintColorMap;
     int result = printColorMap(ReferenceManualFunPtr);
     assert(result == 25);
-    assert(Manual[2].pairNumber == 2);
-    assert(strcmp(Manual[2].MajorColor,"White") == 0);
-    assert(strcmp(Manual[2].MinorColor,"Orange") == 0);
+    assert(Manual[3].pairNumber == 3);
+    assert(strcmp(Manual[3].MajorColor,"White") == 0);
+    assert(strcmp(Manual[3].MinorColor,"Green") == 0);
     printf("All is well (maybe!)\n");
     return 0;
 }
